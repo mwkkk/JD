e6b7cd2cbf5abb6db633bf7b017443d14265cdf0
haixingxing <234845837@qq.com>
2018-06-11 21:52:40 +0800

    init

---
+++ app/js/home/index.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+    <div :class="$style.home">
+        <h1>Home</h1>
+        <input type="text" name="" value="" v-modefont-familyl="msg" @keydown="get">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      msg:''
+    }
+  },
+  watch:{
+    msg(){
+      alert('watch')
+    }
+  },
+  methods:{
+    get(){
+      alert('method')
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 80px;
+  }
+</style>
