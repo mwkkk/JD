aee7f3111eebd9d4571cc984e6383c6f00e58d7d
haixingxing <234845837@qq.com>
2018-06-12 14:58:50 +0800

    init4

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,11 +1,7 @@
 <template lang="html">
     <div :class="$style.home">
         <h1>Home</h1>
-<<<<<<< HEAD
         <input type="text" name="" value="" @keydown="get">
-=======
-        <input type="text" name="" value="" v-modefont-familyl="msg" @keydown="get">
->>>>>>> e6b7cd2cbf5abb6db633bf7b017443d14265cdf0
         {{msg}}
     </div>
 </template>
